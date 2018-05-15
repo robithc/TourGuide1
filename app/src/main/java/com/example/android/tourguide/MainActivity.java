3176add54ae445ac76ef2114af46691ed31d5c76
robithc <robithc@gmail.com>
2018-05-15 05:48:36 +0300

    Revert "Revert "before review""
    This reverts commit c0afde4

--- app/src/main/java/com/example/android/tourguide/MainActivity.java
+++ app/src/main/java/com/example/android/tourguide/MainActivity.java
@@ -1,10 +1,9 @@
 package com.example.android.tourguide;
 
-import android.content.Context;
+import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,11 +27,6 @@ public class MainActivity extends AppCompatActivity {
         TabLayout tabLayout = findViewById(R.id.tabs);
 
         // Connect the tab layout with the view pager. This will
-        //   1. Update the tab layout when the view pager is swiped
-        //   2. Update the view pager when a tab is selected
-        //   3. Set the tab layout's tab names with the view pager's adapter's titles
-        //      by calling onPageTitle()
         tabLayout.setupWithViewPager(viewPager);
-
     }
 }
--- app/src/main/java/com/example/android/tourguide/PlaceAdapter.java
+++ app/src/main/java/com/example/android/tourguide/PlaceAdapter.java
@@ -1,17 +1,12 @@
 package com.example.android.tourguide;
 
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import java.util.ArrayList;
 
 public class PlaceAdapter extends ArrayAdapter<Place> {
@@ -28,35 +23,26 @@ public class PlaceAdapter extends ArrayAdapter<Place> {
                     R.layout.place_item, parent, false);
         }
 
+        //Create Place currentPlace  to get current item in list of places
         final Place currentPlace = getItem(position);
 
-
-
+        //Create imageButton for place image
         ImageButton placeImageButton = listItemView.findViewById(R.id.place_image);
+        //Set it to resource from list of places
         placeImageButton.setImageResource(currentPlace.getPlaceImage());
 
+        //Create textView for name of the place
         TextView placeName = listItemView.findViewById(R.id.place_name_text);
+        //Set it to resource from list of places
         placeName.setText(currentPlace.getPlaceName());
 
+        //Create textView for location of the place
         TextView placeLocation = listItemView.findViewById(R.id.place_location_text);
+        //Set it to resource from list of places
         placeLocation.setText(currentPlace.getPlaceLocation());
-
-        //TextView placeDescription = listItemView.findViewById(R.id.place_description_text);
-        //placeDescription.setText(currentPlace.getPlaceDescription());
-
-
-
-       //ImageButton goToMapButton = listItemView.findViewById(R.id.go_to_map_button);
-       //goToMapButton.setImageResource(R.drawable.ic_place_black_24dp);
-
-
 
         return listItemView;
 }
-
-
-
-
 }
 
 
